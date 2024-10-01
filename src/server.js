58cfaa52c2a3420e5c210353b488b28261f07566
joaovic <joaovictorca2004@gmail.com>
2024-10-01 20:53:46 -0300

    alterando rota da imagem em `app.js`

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import app from './app';
 
-app.listen(process.env.APP_PORT);
+app.listen(process.env.APP_PORT, () => {
+  console.log(`api_rest: ${process.env.APP_URL_DEV ? process.env.APP_URL_DEV : process.env.APP_URL}`);
+});
