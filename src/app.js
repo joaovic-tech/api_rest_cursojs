6aef76fa4f36539dc4df07884dbcc41591d03fc4
joaovic <joaovictorca2004@gmail.com>
2024-09-30 22:46:42 -0300

    alterando rota da imagem em `app.js`

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ class App {
       extended: true,
     }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, '..', '..', 'uploads', 'images')));
+    this.app.use(express.static(resolve(__dirname, '..', '..', 'uploads')));
   }
 
   routes() {
